ead8361c643f296782d9f62801634f9ca3cc4670
ez-li <eileenli.sf@gmail.com>
2019-07-15 16:26:10 -0700

    finished stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,57 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  // takes in data and returns the data in string form
+  // data types:
+  // number, string, boolean, null
+  // function, undefined -> return undefined
+  // array, object
+  var result = '';
+  var type = typeof obj;
+
+  if (type === 'number') {
+    result += obj;
+  }
+  if (type === 'string') {
+    result += '"' + obj + '"';
+  }
+  if (type === 'boolean') {
+    result += obj;
+  }
+  if (obj === null) {
+    return 'null';
+  }
+  if (type === 'function' || obj === undefined) {
+    return;
+  }
+
+  if (Array.isArray(obj)) {
+    result += '[';
+    for (var i = 0; i < obj.length; i++) {
+      if (i + 1 === obj.length) {
+        result += stringifyJSON(obj[i]);
+      } else {
+        result += stringifyJSON(obj[i]) + ',';
+      }
+    }
+    result += ']';
+  }
+  else if (type === 'object') {
+    result += '{';
+    var count = 0;
+    for (var key in obj) {
+      if (obj[key] === 'function' || obj[key] === undefined) {
+        return '{}';
+      }
+      if (count + 1 === Object.keys(obj).length) {
+        result += stringifyJSON(key) + ':' + stringifyJSON(obj[key]);
+      } else {
+        result += stringifyJSON(key) + ':' + stringifyJSON(obj[key]) + ',';
+      }
+      count++;
+    }
+    result += '}';
+  }
+
+  return result;
 };
